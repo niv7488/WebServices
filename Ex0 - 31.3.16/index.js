679e0ae97a6554c52550afe5c04c016cf2b36ada
niv7488 <niv7488@gmail.com>
2016-04-06 20:33:50 +0300

    Return the actions in the body
    and delete the fireEvants module (not necessary)

--- Ex0 - 31.3.16/index.js
+++ Ex0 - 31.3.16/index.js
@@ -1,25 +1,38 @@
 
 var accountSettings = require('./accountSettings');
-var fireEvents = require ('./fire_events');
 var http = require('http');
 
-
+var myArray = [];
 var hotel = accountSettings();
 
-hotel.on("ratingChanged", fireEvents.displayRating);
-hotel.on("ratingChanged", fireEvents.checkUnderRating);
 hotel.on("ratingChanged", function() {
-    fireEvents.checkGoal(this,1000);
+    console.log("Hotel rating is: " + this.rating);
+    myArray.push("Hotel rating is: " + this.rating);
 });
-
+hotel.on("ratingChanged", function() {
+    if(this.rating < 0) {
+        console.log("Hotel under rating in: " + this.rating);
+        myArray.push("Hotel under rating in: " + this.rating);
+    }
+});
+hotel.on("ratingChanged", function() {
+    if(this.rating > 1000) {
+        console.log("goal Achived : " + this.rating);
+        myArray.push("goal Achived : " + this.rating);
+    }
+});
 
 http.createServer(function (req,res) {
     res.writeHeader(200, {'Contenct-Type': 'text/olain'});
-    res.end("Success\n");
-	hotel.addStars(220);
-	hotel.addStars(320);
-	hotel.addStars(600);
-	hotel.reduceStars(700);
+    hotel.addStars(220);
+    hotel.addStars(320);
+    hotel.addStars(600);
+    hotel.reduceStars(400);
+    var response = "Success\n";
+    for(var i = 0; i < myArray.length; ++i){
+        response += myArray[i] + "\n";
+    }
+    res.end(response);
 }).listen(3000,'127.0.0.1');
 
 console.log('listening on port 3000.....');
